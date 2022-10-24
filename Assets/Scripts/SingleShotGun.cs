82fbd56b39bfa26068fa8b0d42f15aeee926c85b
giyeonna@gmail.com <giyeonna@gmail.com>
2022-10-25 00:45:32 +0900

    Add : Bullet Impacts

--- Assets/Scripts/SingleShotGun.cs
+++ Assets/Scripts/SingleShotGun.cs
@@ -1,10 +1,20 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.iOS;
 
 public class SingleShotGun : Gun
 {
     [SerializeField] Camera cam;
+    PhotonView pv;
+
+    private void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
+
     public override void Use()
     {
         Debug.Log("using gu");
@@ -17,6 +27,19 @@ public class SingleShotGun : Gun
         if(Physics.Raycast(ray,out RaycastHit hit))
         {
             hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
+            pv.RPC("RPC_Shoot", RpcTarget.All, hit.point, hit.normal);
+        }
+    }
+
+    [PunRPC]
+    void RPC_Shoot(Vector3 hitPosition, Vector3 hitNormal)
+    {
+        Collider[] colliders = Physics.OverlapSphere(hitPosition, 0.3f);
+        if(colliders.Length != 0)
+        {
+            GameObject bulletImpactObj = Instantiate(bulletImpactPrefab, hitPosition + hitNormal * 0.001f, Quaternion.LookRotation(hitNormal, Vector3.up) * bulletImpactPrefab.transform.rotation);
+            Destroy(bulletImpactObj, 10f);
+            bulletImpactObj.transform.SetParent(colliders[0].transform);
         }
     }
 }
